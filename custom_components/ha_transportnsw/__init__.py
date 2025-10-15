7ca42a1c00e32d01f527b009b36c34800de8afc9
Andy Stewart <andy.stewart@live.com>
2025-10-16 07:39:33 +1100

    All-new version 2.0!

--- custom_components/ha_transportnsw/__init__.py
+++ custom_components/ha_transportnsw/__init__.py
@@ -1,1 +1,57 @@
-"""The ha_transportnsw component."""
+"""The Integration 101 Template integration."""
+
+from __future__ import annotations
+
+from collections.abc import Callable
+from dataclasses import dataclass
+import logging
+
+from homeassistant.config_entries import ConfigEntry, ConfigSubentry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+from homeassistant.helpers.device_registry import DeviceEntry
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+
+from .coordinator import ExampleCoordinator
+
+_LOGGER = logging.getLogger(__name__)
+
+#PLATFORMS: list[Platform] = [Platform.SENSOR]
+PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.DEVICE_TRACKER]
+
+type MyConfigEntry = ConfigEntry[RuntimeData]
+
+
+@dataclass
+class RuntimeData:
+    """Class to hold your data."""
+
+    coordinator: DataUpdateCoordinator
+
+
+async def async_setup_entry(hass: HomeAssistant, config_entry: MyConfigEntry) -> bool:
+    """Set up Example Integration from a config entry."""
+
+    # Initialise the coordinator that manages data updates from your api.
+    # This is defined in coordinator.py
+    coordinator = ExampleCoordinator(hass, config_entry)
+
+    # Add the coordinator and update listener to config runtime data to make
+    # accessible throughout your integration
+    config_entry.runtime_data = RuntimeData(coordinator)
+
+    # Setup platforms (based on the list of entity types in PLATFORMS defined above)
+    # This calls the async_setup method in each of your entity type files.
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+
+    # Return true to denote a successful setup.
+    return True
+
+
+
+async def async_unload_entry(hass: HomeAssistant, config_entry: MyConfigEntry) -> bool:
+    """Unload a config entry."""
+
+    # Unload platforms and return result
+    return await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS)
