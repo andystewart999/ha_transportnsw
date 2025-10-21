9d10edcc61bb5c54c4c728c18a6eb5b21500ce90
Andy Stewart <andy.stewart@live.com>
2025-10-21 21:22:52 +1100

    Refactor sensor options and add removal functions
    Refactor sensor creation logic and add entity/device removal functions.

--- custom_components/ha_transportnsw/helpers.py
+++ custom_components/ha_transportnsw/helpers.py
@@ -8,20 +8,12 @@ import pytz
 import tzlocal
 import time
 from datetime import date, datetime
+from homeassistant.helpers import entity_registry as er
+from homeassistant.helpers import device_registry as dr
 
 _LOGGER = logging.getLogger(__name__)
 
 from .const import *
-
-# def convert_date(utc_string) -> datetime:
-    # fmt = '%Y-%m-%dT%H:%M:%SZ'
-    
-    # utc_dt = datetime.strptime(utc_string, fmt)
-    # utc_dt = utc_dt.replace(tzinfo=pytz.utc)
-    # local_timezone = tzlocal.get_localzone()
-    # local_dt = utc_dt.astimezone(local_timezone)
-    
-    # return local_dt
 
 def get_trips (api_key: str, name_origin: str, name_destination: str, journey_wait_time: int = 0, origin_transport_type: int = [0], destination_transport_type: int = [0],
               strict_transport_type: bool = False, route_filter: str = '', journeys_to_return: int = 1, include_realtime_location: bool = True, 
@@ -80,31 +72,47 @@ def get_stop_detail (stop_data, stop_id: str, property: str):
         return "n/a"
 
 
-def set_optional_sensors (sensor_data: str):
+def set_optional_sensors (sensor_creation: str):
     # Determine which optional sensors to create
-    sensor_data = sensor_data.lower()
 
-    # First reset what sensors we're creating
-    for sensor_group in [BASIC_SENSORS, MEDIUM_SENSORS, VERBOSE_SENSORS]:
-        for sensor in sensor_group:
-            sensor = False
-    
-    # Then turn specific pre-defined sensor groups back on based on what the user selected
-    if sensor_data == 'basic':
-        for sensor_group in [BASIC_SENSORS]:
-            for sensor in sensor_group:
-                sensor = True
-    
-    elif sensor_data == 'medium':
-        for sensor_group in [BASIC_SENSORS, MEDIUM_SENSORS]:
-            for sensor in sensor_group:
-                sensor = True
-    
-    elif return_data == 'verbose':
-        for sensor_group in [BASIC_SENSORS, MEDIUM_SENSORS, VERBOSE_SENSORS]:
-            for sensor in sensor_group:
-                sensor = True
+    # These are for the new integration
+    if sensor_creation == 'changes_and_times':
+        sensor_options = {
+                        'time_and_change_sensors': {CONF_CHANGES_SENSOR: True, CONF_DELAY_SENSOR: True, CONF_FIRST_LEG_DEPARTURE_TIME_SENSOR: True, CONF_LAST_LEG_ARRIVAL_TIME_SENSOR: True},
+                        'origin_sensors': {CONF_ORIGIN_NAME_SENSOR: False, CONF_ORIGIN_DETAIL_SENSOR: False, CONF_FIRST_LEG_LINE_NAME_SENSOR: False, CONF_FIRST_LEG_LINE_NAME_SHORT_SENSOR: False, CONF_FIRST_LEG_TRANSPORT_TYPE_SENSOR: False, CONF_FIRST_LEG_TRANSPORT_NAME_SENSOR: False, CONF_FIRST_LEG_OCCUPANCY_SENSOR: False, CONF_FIRST_LEG_DEVICE_TRACKER: DEFAULT_FIRST_LEG_DEVICE_TRACKER}, 
+                        'destination_sensors': {CONF_DESTINATION_NAME_SENSOR: False, CONF_ORIGIN_DETAIL_SENSOR: False, CONF_LAST_LEG_LINE_NAME_SENSOR: False, CONF_LAST_LEG_LINE_NAME_SHORT_SENSOR: False, CONF_LAST_LEG_OCCUPANCY_SENSOR: False, CONF_LAST_LEG_DEVICE_TRACKER: DEFAULT_LAST_LEG_DEVICE_TRACKER}
+                        }
 
+    elif sensor_creation == 'verbose':
+        sensor_options = {
+                'time_and_change_sensors': {CONF_CHANGES_SENSOR: True, CONF_DELAY_SENSOR: True, CONF_FIRST_LEG_DEPARTURE_TIME_SENSOR: True, CONF_LAST_LEG_ARRIVAL_TIME_SENSOR: True},
+                'origin_sensors': {CONF_ORIGIN_NAME_SENSOR: True, CONF_ORIGIN_DETAIL_SENSOR: True, CONF_FIRST_LEG_LINE_NAME_SENSOR: True, CONF_FIRST_LEG_LINE_NAME_SHORT_SENSOR: True, CONF_FIRST_LEG_OCCUPANCY_SENSOR: True, CONF_FIRST_LEG_DEVICE_TRACKER: True}, 
+                'destination_sensors': {CONF_DESTINATION_NAME_SENSOR: True, CONF_DESTINATION_DETAIL_SENSOR: True, CONF_LAST_LEG_LINE_NAME_SENSOR: True, CONF_LAST_LEG_LINE_NAME_SHORT_SENSOR: True, CONF_LAST_LEG_OCCUPANCY_SENSOR:True, CONF_LAST_LEG_DEVICE_TRACKER: DEFAULT_LAST_LEG_DEVICE_TRACKER}
+                }
+ 
+    # These are for migration entries
+    elif sensor_creation == 'basic':
+        sensor_options = {
+                        'time_and_change_sensors': {CONF_CHANGES_SENSOR: True, CONF_DELAY_SENSOR: False, CONF_FIRST_LEG_DEPARTURE_TIME_SENSOR: False, CONF_LAST_LEG_ARRIVAL_TIME_SENSOR: True},
+                        'origin_sensors': {CONF_ORIGIN_NAME_SENSOR: False, CONF_ORIGIN_DETAIL_SENSOR: False, CONF_FIRST_LEG_LINE_NAME_SENSOR: False, CONF_FIRST_LEG_LINE_NAME_SHORT_SENSOR: False, CONF_FIRST_LEG_OCCUPANCY_SENSOR: False, CONF_FIRST_LEG_DEVICE_TRACKER: True}, 
+                        'destination_sensors': {CONF_DESTINATION_NAME_SENSOR: False, CONF_DESTINATION_DETAIL_SENSOR: False, CONF_LAST_LEG_LINE_NAME_SENSOR: False, CONF_LAST_LEG_LINE_NAME_SHORT_SENSOR: False, CONF_LAST_LEG_OCCUPANCY_SENSOR: False, CONF_LAST_LEG_DEVICE_TRACKER: 'always'}
+                    }
+ 
+    elif sensor_creation == 'medium':
+        sensor_options = {
+                        'time_and_change_sensors': {CONF_CHANGES_SENSOR: True, CONF_DELAY_SENSOR: False, CONF_FIRST_LEG_DEPARTURE_TIME_SENSOR: False, CONF_LAST_LEG_ARRIVAL_TIME_SENSOR: True},
+                        'origin_sensors': {CONF_ORIGIN_NAME_SENSOR: True, CONF_ORIGIN_DETAIL_SENSOR: True, CONF_FIRST_LEG_LINE_NAME_SENSOR: False, CONF_FIRST_LEG_LINE_NAME_SHORT_SENSOR: False, CONF_FIRST_LEG_OCCUPANCY_SENSOR: False, CONF_FIRST_LEG_DEVICE_TRACKER: True}, 
+                        'destination_sensors': {CONF_DESTINATION_NAME_SENSOR: False, CONF_DESTINATION_DETAIL_SENSOR: True, CONF_LAST_LEG_LINE_NAME_SENSOR: False, CONF_LAST_LEG_LINE_NAME_SHORT_SENSOR: False, CONF_LAST_LEG_OCCUPANCY_SENSOR: False, CONF_LAST_LEG_DEVICE_TRACKER: 'always'}
+                    }
+  
+    else:
+        sensor_options = {
+                        'time_and_change_sensors': {CONF_CHANGES_SENSOR: False, CONF_DELAY_SENSOR: False, CONF_FIRST_LEG_DEPARTURE_TIME_SENSOR: False, CONF_LAST_LEG_ARRIVAL_TIME_SENSOR: False},
+                        'origin_sensors': {CONF_ORIGIN_NAME_SENSOR: False, CONF_ORIGIN_DETAIL_SENSOR: False, CONF_FIRST_LEG_LINE_NAME_SENSOR: False, CONF_FIRST_LEG_LINE_NAME_SHORT_SENSOR: False, CONF_FIRST_LEG_OCCUPANCY_SENSOR: False, CONF_FIRST_LEG_DEVICE_TRACKER: DEFAULT_FIRST_LEG_DEVICE_TRACKER}, 
+                        'destination_sensors': {CONF_DESTINATION_NAME_SENSOR: False, CONF_DESTINATION_DETAIL_SENSOR: False, CONF_LAST_LEG_LINE_NAME_SENSOR: False, CONF_LAST_LEG_LINE_NAME_SHORT_SENSOR: False, CONF_LAST_LEG_OCCUPANCY_SENSOR: False, CONF_LAST_LEG_DEVICE_TRACKER: DEFAULT_LAST_LEG_DEVICE_TRACKER}
+                    }
+
+    return sensor_options
 
 def get_api_calls (file_path: str) -> int:
     # Get the current date first
@@ -151,6 +159,31 @@ def set_api_calls (file_path: str, api_calls: int) -> int:
     Path(file_path).write_text(json.dumps(data), encoding="utf8")
 
     return api_calls
-        
-    
-    
+
+def remove_entity(entity_reg, configentry_id, subentry_id,trip_index, key):
+    # Search for and remove a sensor that's no longer needed
+    unique_id = f"{subentry_id}_{key}_{trip_index}"
+
+    try:
+        # Get all the entities for this config entry
+        entities = entity_reg.entities.get_entries_for_config_entry_id(configentry_id)
+
+        # Search for the one to remove
+        for entity in entities:
+            if entity.unique_id == unique_id:
+                entity_reg.async_remove(entity.entity_id)
+                break
+
+    finally:
+        # Don't log an error as it's possible the entity never existed in the first place
+        pass
+
+def remove_device(device_reg, subentry_id, origin_id, destination_id, device_identifier):
+    # Search for and remove a device that's no longer needed
+    try:
+        device = device_reg.async_get_device(identifiers={(DOMAIN, f"{subentry_id}_{origin_id}_{destination_id}_{device_identifier}")})
+        if device is not None:
+            device_reg.async_remove_device(device.id)
+
+    finally:
+        pass
